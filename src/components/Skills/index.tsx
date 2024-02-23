4b697896ad810c4a79379c036679c4db250d3731
Will <willwow@outlook.com>
2024-02-23 17:30:52 -0300

    Update: Skills card

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -19,12 +19,12 @@ const Skills = () => {
             <h3 className='text-gradient text-2xl text-center font-bold mb-5'>Ferramentas</h3>
             <Swiper
                 spaceBetween={50}
-                slidesPerView={3.75}
+                slidesPerView={2.5}
                 className='lg:hidden'
             >
                 {Skill.map((skill, index) => (
                     <SwiperSlide key={index} className='flex'>
-                        <div className='flex flex-col items-center justify-center'>
+                        <div className='w-full flex flex-col items-center justify-center border border-ice-blue border-opacity-50 rounded p-2'>
                             <div className='flex h-10'>
                                 <Image src={skill.image} width={36} height={36} className='mb-1' alt='' />
                             </div>
@@ -36,7 +36,7 @@ const Skills = () => {
             <div>
                 <div className='hidden lg:flex justify-center'>
                     {Skill.map((skill, index) => (
-                        <div key={index} className='flex flex-col items-center justify-center me-10'>
+                        <div key={index} className='flex flex-col items-center justify-center bg-black border border-ice-blue border-opacity-50 hover:border-opacity-100 hover:-translate-y-1 rounded me-10 p-3'>
                             <div className='flex h-10'>
                                 <Image src={skill.image} width={64} height={64} className='mb-1' alt='' />
                             </div>
